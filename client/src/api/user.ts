4518074ebe37f6053bce7789fdda3f9aac1aaed1
d_flex <hiyoo95@gmail.com>
2022-04-08 00:00:10 +0900

    재배포

--- client/src/api/user.ts
+++ client/src/api/user.ts
@@ -23,7 +23,7 @@ export const logUserIn = (userInfo: { email: string; password: string }) => {
 };
 
 const KAKAO_REDIRECT_URI =
-  'http://elice-kdt-ai-3rd-team08.elicecoding.com/user/kakao/callback';
+  'http://chae-recipe.koreacentral.cloudapp.azure.com/user/kakao/callback';
 const HOST = 'https://kauth.kakao.com';
 const REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
 
@@ -37,7 +37,7 @@ export const sendKakaoAuthCode = (authCode: string | null) => {
 };
 
 const GOOGLE_REDIRECT_URI =
-  'https://elice-kdt-ai-3rd-team08.elicecoding.com/user/google/callback';
+  'https://chae-recipe.koreacentral.cloudapp.azure.com/user/google/callback';
 const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_KEY;
 const SCOPE = 'openid%20profile%20email';
 
